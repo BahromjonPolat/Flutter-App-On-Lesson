2aefd68947e5b8e07293f82f8e2e7c72ce0e6996
BahromjonPolat <itjunior.uz@gmail.com>
2021-08-14 21:07:24 +0500

    Profile UI page was made

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/ui/body_page.dart';
+import 'package:my_flutter_app/ui/profile_page.dart';
 
-void main() => runApp(MyApp());
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.deepOrange,
-          accentColor: Colors.orangeAccent,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Flutter App"),
-          elevation: 12.0,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Icon(Icons.search),
-            ),
-          ],
-        ),
-        body: BodyPage(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-
-          },
-        ),
-      ),
-
+      home: ProfilePage(),
     );
   }
 }
