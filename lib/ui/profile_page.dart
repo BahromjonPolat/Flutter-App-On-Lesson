c6041db16e8ea7a2053c01b74d5c1c88a85c41b1
BahromjonPolat <itjunior.uz@gmail.com>
2021-08-15 18:52:46 +0500

    Profile UI yakunlandi

--- lib/ui/profile_page.dart
+++ lib/ui/profile_page.dart
@@ -26,48 +26,148 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Center(
         child: Column(
           children: <Widget>[
-            Image(
-              width: 64.0,
-              height: 64.0,
-              image: AssetImage("images/123.jpg"),
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Image(
+                width: 120.0,
+                height: 120.0,
+                image: AssetImage("images/123.jpg"),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                OutlinedButton(
-                  child: Row(
-                    children: [
-                      Icon(Icons.edit),
-                      Text("Edit"),
-                    ],
+            Text("Bahromjon Po`lat"),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton(
+                    child: Row(
+                      children: [
+                        Icon(Icons.edit),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Text("Edit"),
+                      ],
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
-
-                SizedBox(
-
-                ),
-
-                OutlinedButton(
-                  child: Row(
-                    children: [
-                      Icon(Icons.edit),
-                      Text("Edit"),
-                    ],
+                  SizedBox(
+                    height: 24.0,
+                    width: 24.0,
+                    child: VerticalDivider(
+                      color: Colors.green,
+                    ),
                   ),
-                  onPressed: () {},
-                ),
-
-                OutlinedButton(
-                  child: Row(
-                    children: [
-                      Icon(Icons.edit),
-                      Text("Edit"),
-                    ],
+                  OutlinedButton(
+                    child: Row(
+                      children: [
+                        Icon(Icons.attach_money_sharp),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Text("TopUp"),
+                      ],
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
+                  SizedBox(
+                    height: 24.0,
+                    width: 24.0,
+                    child: VerticalDivider(
+                      color: Colors.green,
+                    ),
+                  ),
+                  OutlinedButton(
+                    child: Row(
+                      children: [
+                        Icon(Icons.mobile_screen_share_outlined),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Text("Invite"),
+                      ],
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(16.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "+998 93 188 1333",
+                          textAlign: TextAlign.start,
+                        ),
+                        Text(
+                          "Mobile Phone",
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                      child: VerticalDivider(
+                        color: Colors.green,
+                      ),
+                    ),
+                    Icon(
+                      Icons.phone,
+                      color: Colors.green,
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                      child: VerticalDivider(
+                        color: Colors.green,
+                      ),
+                    ),
+                    Icon(
+                      Icons.messenger,
+                      color: Colors.green,
+                    ),
+                  ],
                 ),
-              ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(16.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Text("+998 93 188 1333"),
+                        Text("Home"),
+                      ],
+                    ),
+
+                    SizedBox(
+                      width: 128.0,
+                    ),
+
+                    SizedBox(
+                      height: 24.0,
+                      width: 32.0,
+                      child: VerticalDivider(
+                        color: Colors.green,
+                      ),
+                    ),
+                    Icon(
+                      Icons.phone,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
